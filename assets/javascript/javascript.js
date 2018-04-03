260099e98f7aa1c3f7acdf4c5ed93da83dae7e3a
npasher <n.p.asher@gmail.com>
2018-04-03 13:32:29 -0400

    initial

---
+++ assets/javascript/javascript.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+  var config = {
+    apiKey: "AIzaSyCKydCfuiJkyNh-eb3VixxjJSjnkYccOmQ",
+    authDomain: "train-scheduler-5896f.firebaseapp.com",
+    databaseURL: "https://train-scheduler-5896f.firebaseio.com",
+    projectId: "train-scheduler-5896f",
+    storageBucket: "",
+    messagingSenderId: "484178492119"
+  };
+  firebase.initializeApp(config);
+  
+});
